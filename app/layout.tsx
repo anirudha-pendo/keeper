e8fd9763a17763e6c536ea26e526278eb7ef1ea2
Anirudha Gandhare <anirudha.gandhare@pendo.io>
2026-06-05 16:54:09 +0530

    replace apiKey with window.platformGlobalKey

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
     o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
     y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo-dev.pendo-dev.com/agent/static/'+apiKey+'/pendo.js';
     z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-})('b7a895ee-6fba-49e3-8cbd-bfd8e0db68a7');
+})('925c0c93-cc85-43ac-8d61-dde268c5b819');
 `,
           }}
         />
